7034682552963af8456bbae9c1242d190408c0e0
Arul Murugan <arulmurugan.rama@aspiresys.com>
2023-12-18 19:52:12 +0530

    UPDATE: File structure changes

--- src/test/app.component.spec.ts
+++ src/test/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
+import { AppComponent } from '../app/app.component';
+import { SharedModule } from '../app/shared/shared.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
