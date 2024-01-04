9f9956d73dcd55aab6408b1fd34a4e94c005aa36
Arul Murugan <arulmurugan.rama@aspiresys.com>
2024-01-04 12:09:01 +0530

    UPDATE: view-product removed

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { CreateProductsComponent } from './create-products/create-products.component';
-import { ViewProductsComponent } from './view-products/view-products.component';
 
 const routes: Routes = [
   { path: 'create', component: CreateProductsComponent },
-  { path: 'view', component: ViewProductsComponent },
 ];
 
 @NgModule({
